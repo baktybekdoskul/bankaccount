ba58bca86a776c9ea03846787557bbb64e50e41b
baktybekdoskul <baktybek.doskul@gmail.com>
2022-07-07 06:04:56 +0600

    #17 Detailed tests, mock test

--- src/main/kotlin/com/example/bankaccount/domain/account/AccountRepository.kt
+++ src/main/kotlin/com/example/bankaccount/domain/account/AccountRepository.kt
@@ -10,6 +10,6 @@ import javax.persistence.LockModeType
 @Repository
 interface AccountRepository : JpaRepository<AccountEntity, Long> {
     @Transactional
-    @Lock(value = LockModeType.PESSIMISTIC_WRITE) // TODO Pessimistic read or write
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     override fun findById(id: Long): Optional<AccountEntity>
 }
